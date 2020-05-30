3ff4f7313795039cef7c85abc4a6253078e74fb1
Isma <didouhismael@gmail.com>
2020-05-30 14:38:15 +0200

    enable navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,1 +1,16 @@
-rc
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand-logo">
+          MarioPlan
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
