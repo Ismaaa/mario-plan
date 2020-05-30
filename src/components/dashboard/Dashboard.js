4f2b3c64e6b8c3f6672d4a303c1d15449a8b4e99
Isma <didouhismael@gmail.com>
2020-05-31 00:23:14 +0200

    refactor dashboard to a funcitonal component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,22 +1,23 @@
-import React, { Component } from "react";
+// livs
+import React from "react";
+
+// components
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div className="dashboard container">
-        <div className="row">
-          <div className="col s12 m6">
-            <ProjectList />
-          </div>
-          <div className="col s12 m5 offset-m1">
-            <Notifications />
-          </div>
+const Dashboard = () => {
+  return (
+    <div className="dashboard container">
+      <div className="row">
+        <div className="col s12 m6">
+          <ProjectList />
+        </div>
+        <div className="col s12 m5 offset-m1">
+          <Notifications />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Dashboard;
