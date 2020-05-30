33a1f6833a84f73ea600e65acc987a9e4b296f9c
Isma <didouhismael@gmail.com>
2020-05-31 00:57:26 +0200

    shorter placeholders

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -4,19 +4,19 @@ const INITIAL_STATE = {
       id: 1,
       title: "Help me find peach",
       content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis officia non expedita vel. Facilis vitae minus dolorem eos corporis. Provident sit omnis sunt doloremque distinctio suscipit harum optio ipsum quidem.",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis officia non expedita vel. Facilis vitae minus dolorem eos corporis.",
     },
     {
       id: 2,
       title: "Collect all the stars",
       content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni facere, obcaecati consectetur omnis laboriosam maxime nesciunt ipsum laudantium incidunt saepe quibusdam assumenda autem! Fuga exercitationem repudiandae, autem impedit animi maxime.",
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni facere, obcaecati consectetur omnis laboriosam maxime nesciunt ipsum laudantium incidunt saepe quibusdam assumenda autem!",
     },
     {
       id: 3,
       title: "Egg hunt with yoshi",
       content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dolorum ducimus sapiente. Enim esse sequi veniam assumenda perferendis ipsam, est aliquid facilis aspernatur illo odit quia cum ratione debitis nobis.",
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dolorum ducimus sapiente.",
     },
   ],
 };
