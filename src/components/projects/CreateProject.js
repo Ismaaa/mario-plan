b9dc7201e0f2d52c832cbe55a253ae766f57a59a
Isma <didouhismael@gmail.com>
2020-05-31 01:17:50 +0200

    refactor the whole create project to a functional component + use hooks instead

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,56 +1,65 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
 
-class CreateProject extends Component {
-  constructor() {
-    super();
-    /* eslint-disable react/no-unused-state */
-    this.state = {
-      title: "",
-      content: "",
-    };
-    /* eslint-enable react/no-unused-state */
-  }
+const CreateProject = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value,
-    });
+  const dispatch = useDispatch();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
   };
 
-  handleSubmit = (e) => {
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
     e.preventDefault();
+    dispatch(
+      createProject({
+        title,
+        content,
+      })
+    );
   };
 
-  render() {
-    return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Create a project</h5>
-          <div className="input-field">
-            <label htmlFor="title">Title</label>
-            <input id="title" type="text" onChange={this.handleChange} />
-          </div>
+  return (
+    <div className="container">
+      <form onSubmit={handleFormSubmit} className="white">
+        <h5 className="grey-text text-darken-3">Create a project</h5>
+        <div className="input-field">
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
 
-          <div className="input-field">
-            <label htmlFor="content">Project Content</label>
-            <textarea
-              name="content"
-              id="content"
-              className="materialize-textarea"
-              onChange={this.handleChange}
-            />
-          </div>
+        <div className="input-field">
+          <label htmlFor="content">Project Content</label>
+          <textarea
+            name="content"
+            id="content"
+            className="materialize-textarea"
+            value={content}
+            onChange={handleContentChange}
+          />
+        </div>
 
-          <div className="input-field">
-            <button type="submit" className="btn pink lighten-1 z-depth-0">
-              Create
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        <div className="input-field">
+          <button type="submit" className="btn pink lighten-1 z-depth-0">
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default CreateProject;
