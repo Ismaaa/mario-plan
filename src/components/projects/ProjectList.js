10d2832a76fd95e43ca8d7641b204ca0334b3cd4
Isma <didouhismael@gmail.com>
2020-05-31 00:23:25 +0200

    use redux data in projects list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
+import { useSelector } from "react-redux";
 
 const ProjectList = () => {
+  const projects = useSelector((state) => state.project.projects);
+
   return (
     <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      {projects.map((project) => {
+        const { id, title, content } = project;
+        return (
+          <ProjectSummary key={id} id={id} title={title} content={content} />
+        );
+      })}
     </div>
   );
 };
