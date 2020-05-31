5d1ac839bb123dc4127d2f80d64e0e4d5442992d
Isma <didouhismael@gmail.com>
2020-06-01 01:49:43 +0200

    clean root reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,17 +1,15 @@
 // libs
 import { combineReducers } from "redux";
-import { firebaseReducer } from "react-redux-firebase";
 import { firestoreReducer } from "redux-firestore";
 
-// reducers
+//
 import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
 
 const rootReducer = combineReducers({
-  firebase: firebaseReducer,
-  firestore: firestoreReducer,
   auth: authReducer,
   project: projectReducer,
+  firestore: firestoreReducer,
 });
 
 export default rootReducer;
