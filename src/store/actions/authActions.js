0b2803680cc67d372eb7a68b556f44f56f53a42d
Isma <didouhismael@gmail.com>
2020-06-08 01:06:40 +0200

    format

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -5,13 +5,14 @@ export const signIn = (credentials) => {
   return (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
 
-    firebase.auth().signInWithEmailAndPassword(
-      credentials.email,
-      credentials.password
-    ).then(() => {
-      dispatch({ type: LOGIN_SUCCESS })
-    }).catch((error) => {
-      dispatch({ type: LOGIN_ERROR, error })
-    })
-  }
-}
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: LOGIN_SUCCESS });
+      })
+      .catch((error) => {
+        dispatch({ type: LOGIN_ERROR, error });
+      });
+  };
+};
