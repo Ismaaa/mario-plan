7a04b25571334f60cc288dad1c9beabc3dc28443
Isma <didouhismael@gmail.com>
2020-06-08 21:41:39 +0200

    changed the order the data is displayed in the project list

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -7,8 +7,8 @@ const ProjectSummary = ({ title, content }) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{title}</span>
-        <p>Posted by Ismail Didouh</p>
-        <p className="grey-text">{content}</p>
+        <p>{content}</p>
+        <p className="grey-text">Posted by Ismail Didouh</p>
       </div>
     </div>
   );
