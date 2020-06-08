cf050ddec5d5401cca29ee8840dc69539e49ab85
Isma <didouhismael@gmail.com>
2020-06-08 23:44:38 +0200

    404 page when project not found

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,10 +2,12 @@
 import React, { useState, useEffect } from "react";
 import { useFirestoreConnect } from "react-redux-firebase";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-const ProjectDetails = ({ computedMatch }) => {
-  const { id } = computedMatch.params;
-  const [project, setProject] = useState({});
+const ProjectDetails = ({ match }) => {
+  const { id } = match.params;
+
+  const [project, setProject] = useState();
   const projects = useSelector((state) => state.firestore.data.projects);
 
   useFirestoreConnect(["projects"]);
@@ -16,30 +18,34 @@ const ProjectDetails = ({ computedMatch }) => {
     }
   }, [id, projects]);
 
-  if (project) {
+  if (!project)
     return (
       <div className="container section project-details">
         <div className="card z-depth-0">
           <div className="card-content">
-            <span className="card-title">{project.title}</span>
-            <p>{project.content}</p>
+            <span className="card-title">Project not found</span>
+            <Link className="btn" to="/">
+              Go back to the Dashboard
+            </Link>
           </div>
           <div className="card-action grey lighten-4 grey-text">
-            <div>
-              Posted by {project.authorFirstName} {project.authorLastName}
-            </div>
+            <span>Error 404</span>
           </div>
         </div>
       </div>
     );
-  }
 
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
-        <div className="card-content"></div>
+        <div className="card-content">
+          <span className="card-title">{project.title}</span>
+          <p>{project.content}</p>
+        </div>
         <div className="card-action grey lighten-4 grey-text">
-          Loading project...
+          <div>
+            Posted by {project.authorFirstName} {project.authorLastName}
+          </div>
         </div>
       </div>
     </div>
