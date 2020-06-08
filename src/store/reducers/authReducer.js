5ba7c7939ac9abca613f86a09966131c3d7d7981
Isma <didouhismael@gmail.com>
2020-06-08 22:11:47 +0200

    handle logout on the reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,5 +1,10 @@
 // types
-import { LOGIN_SUCCESS, LOGIN_ERROR } from "../types/authTypes";
+import {
+  LOGIN_SUCCESS,
+  LOGIN_ERROR,
+  LOGOUT_SUCCESS,
+  LOGOUT_ERROR,
+} from "../types/authTypes";
 
 const INITIAL_STATE = {
   authError: null,
@@ -8,11 +13,17 @@ const INITIAL_STATE = {
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case LOGIN_ERROR:
-      console.error(action.error);
+      console.error("Login failed", action.error);
       return { ...state, authError: "Login Failed" };
     case LOGIN_SUCCESS:
       console.log("Login success");
       return { ...state, authError: null };
+    case LOGOUT_SUCCESS:
+      console.log("Logout success");
+      return { ...state, authError: null };
+    case LOGOUT_ERROR:
+      console.error("Logout failed", action.error);
+      return { ...state };
     default:
       return state;
   }
