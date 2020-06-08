a5de1806d31ed3b387cf7959c7edf3926b6d298f
Isma <didouhismael@gmail.com>
2020-06-09 00:51:13 +0200

    removed dummy data

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,30 +1,7 @@
 // types
 import { CREATE_PROJECT, CREATE_PROJECT_ERROR } from "../types/projectTypes";
 
-const INITIAL_STATE = {
-  projects: [
-    {
-      id: 1,
-      title: "Help me find peach",
-      content:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis officia non expedita vel. Facilis vitae minus dolorem eos corporis.",
-    },
-    {
-      id: 2,
-      title: "Collect all the stars",
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni facere, obcaecati consectetur omnis laboriosam maxime nesciunt ipsum laudantium incidunt saepe quibusdam assumenda autem!",
-    },
-    {
-      id: 3,
-      title: "Egg hunt with yoshi",
-      content:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dolorum ducimus sapiente.",
-    },
-  ],
-};
-
-const projectReducer = (state = INITIAL_STATE, action) => {
+const projectReducer = (state, action) => {
   switch (action.type) {
     case CREATE_PROJECT:
       console.log(action.project);
