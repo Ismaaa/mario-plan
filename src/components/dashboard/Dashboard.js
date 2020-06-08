11d12f8c566781a489187607a3f9e28d2b50e60b
Isma <didouhismael@gmail.com>
2020-06-08 22:52:00 +0200

    protect dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,17 @@
 // libs
 import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 // Components
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
 
 const Dashboard = () => {
+  const auth = useSelector((state) => state.firebase.auth);
+
+  if (!auth.uid) return <Redirect to="signin" />;
+
   return (
     <div className="dashboard container">
       <div className="row">
