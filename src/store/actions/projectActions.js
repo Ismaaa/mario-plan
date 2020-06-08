5a66f07294919cb75a4a30987daa4602aa0c7ea8
Isma <didouhismael@gmail.com>
2020-06-09 00:51:42 +0200

    refactor project actions

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,26 +1,24 @@
 /* eslint-disable import/prefer-default-export */
 import { CREATE_PROJECT, CREATE_PROJECT_ERROR } from "../types/projectTypes";
 
-export const createProject = (project) => {
-  return (dispatch, getState, { getFirebase }) => {
-    const firestore = getFirebase().firestore();
-    const { firstName, lastName } = getState().firebase.profile;
-    const { uid } = getState().firebase.auth;
+export const createProject = (project) => (dispatch, getState, { getFirebase }) => {
+  const firestore = getFirebase().firestore();
+  const { firstName, lastName } = getState().firebase.profile;
+  const { uid } = getState().firebase.auth;
 
-    firestore
-      .collection("projects")
-      .add({
-        ...project,
-        authorFirstName: firstName,
-        authorLastName: lastName,
-        authorId: uid,
-        createdAt: new Date(),
-      })
-      .then(() => {
-        dispatch({ type: CREATE_PROJECT, project });
-      })
-      .catch((error) => {
-        dispatch({ type: CREATE_PROJECT_ERROR, error });
-      });
-  };
+  firestore
+    .collection("projects")
+    .add({
+      ...project,
+      authorFirstName: firstName,
+      authorLastName: lastName,
+      authorId: uid,
+      createdAt: new Date(),
+    })
+    .then(() => {
+      dispatch({ type: CREATE_PROJECT, project });
+    })
+    .catch((error) => {
+      dispatch({ type: CREATE_PROJECT_ERROR, error });
+    });
 };
