175a213f0a59dff4ff51ab36a77b62c4eac0fa2b
Isma <didouhismael@gmail.com>
2020-06-09 19:48:07 +0200

    implemented redux dev tools :rocket:

--- src/store/index.js
+++ src/store/index.js
@@ -12,15 +12,17 @@ import firebaseConfig from "../services/firebase/config";
 import rootReducer from "./reducers/rootReducer";
 
 let middlewares = [thunk.withExtraArgument({ getFirebase })];
+let composeEnhancers = compose;
 
 if (process.env.NODE_ENV === "development") {
   const logger = createLogger({ collapsed: true });
   middlewares = [...middlewares, logger];
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(...middlewares), reduxFirestore(firebaseConfig))
+  composeEnhancers(applyMiddleware(...middlewares), reduxFirestore(firebaseConfig))
 );
 
 export default store;
