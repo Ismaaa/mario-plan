6ae077e1071905cc9a2477b991c6e618303dc34d
Isma <didouhismael@gmail.com>
2020-06-21 18:00:31 +0200

    implemented firebase emulator

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -3,11 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 import firebase from "firebase/app";
-
 // config
 import firebaseConfig from "./config";
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
+
+if (process.env.NODE_ENV === 'development') {
+  firebase.firestore().settings({host: "localhost:8080", ssl: false});
+}
 
 export default firebase;
