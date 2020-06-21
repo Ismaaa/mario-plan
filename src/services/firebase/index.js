4a7d5b67611c995029841c9b901145aaf6f83c7b
Isma <didouhismael@gmail.com>
2020-06-21 20:58:48 +0200

    format index

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -3,14 +3,15 @@ import "firebase/auth";
 import "firebase/firestore";
 
 import firebase from "firebase/app";
+
 // config
 import firebaseConfig from "./config";
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
 
-if (process.env.NODE_ENV === 'development') {
-  firebase.firestore().settings({host: "localhost:8080", ssl: false});
+if (process.env.NODE_ENV === "development") {
+  firebase.firestore().settings({ host: "localhost:8080", ssl: false });
 }
 
 export default firebase;
